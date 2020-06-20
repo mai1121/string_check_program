d67f94324e081004699a01494f951c1ed10856ed
mai1121 <maimaiukawa@gmail.com>
2020-06-20 09:56:17 +0900

    first commit

---
+++ string_check_program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace string_check_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
